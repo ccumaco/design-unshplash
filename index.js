85d2d08e717e1140433a2b5ede1214d1a54df9a3
cecumaco <carlos.cumaco@inoutdelivery.com>
2021-04-25 03:41:48 -0500

    nuevas fuentes

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const element = '';
 let crearDiv = document.createElement('div')
 const contentImage = document.getElementById('fotos');
+cantidadImagenes = 10;
 async function cargarImagenes () {
-    for (let index = 0; index < images.length; index++) {
-        contentImage.innerHTML+= `
+    
+        for (let index = 0; index < cantidadImagenes; index++) {
+            contentImage.innerHTML+= `
             <div class="container-img">
-                <img src="${images[index].route}" alt="${images[index].alt}"/>
+             <img src="${images[index].route}" alt="${images[index].alt}"/>
+             <div></div>
             </div>
-        `
-    }
+            `
+        }
 }
 
 function rowImages() {
     contentImage.style.gridTemplateColumns = 'repeat(1, 1fr)'
     console.log('entro');
+}
+function showMore () {
+    cantidadImagenes + 10;
+    if(cantidadImagenes <= images.length){} else {
+        cantidadImagenes = images.length
+    }
+    cargarImagenes()
 }
 
 cargarImagenes()
